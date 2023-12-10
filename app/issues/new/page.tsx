b28b6b62e4a0890985aed085672390e57332d095
briha <hello@briha.xyz>
2023-12-10 18:58:09 +0530

    completed form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,20 +1,44 @@
 "use client";
 
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button, TextField } from "@radix-ui/themes";
+import "easymde/dist/easymde.min.css";
 import React from "react";
+import { useForm, Controller } from "react-hook-form";
+import SimpleMde from "react-simplemde-editor";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
-type pageProps = {};
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
-const NewIssuePage: React.FC<pageProps> = () => {
+const NewIssuePage: React.FC = () => {
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const router = useRouter();
+
   return (
-    <div className="max-w-xl space-y-7">
+    <form
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+      className="max-w-xl space-y-7"
+    >
       <TextField.Root>
-        <TextField.Input placeholder="Title" />
+        <TextField.Input placeholder="Title" {...register("title")} />
       </TextField.Root>
-      <TextArea rows={5} variant="soft" placeholder="Description" />
+      {/* you can't use {...register("title")} for the simpleMDE as it doesn't allow any extra props, so we need to use the Controller from React Hook Form */}
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMde placeholder="Description" {...field} />
+        )}
+      ></Controller>
 
       <Button>Submit issue</Button>
-    </div>
+    </form>
   );
 };
 export default NewIssuePage;
