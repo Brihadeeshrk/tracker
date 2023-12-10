9abf10a0f88c157da84fe3a1cc14bfefa42f0c1b
briha <hello@briha.xyz>
2023-12-10 23:55:55 +0530

    client side validation complete

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button, Callout, Link, TextField } from "@radix-ui/themes";
+import { Button, Callout, Link, Text, TextField } from "@radix-ui/themes";
 import "easymde/dist/easymde.min.css";
 import React, { useState } from "react";
 import { useForm, Controller } from "react-hook-form";
@@ -8,21 +8,38 @@ import SimpleMde from "react-simplemde-editor";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { CiCircleInfo } from "react-icons/ci";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { z } from "zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+/**
+ * In the previous implementation where we had an interface called IssueForm with 2 fields,
+ * any change to the form, would have to be made in 2 separate files, one over here and one in validationSchemas.ts
+ * to get rid of this redundancy, we can derive this IssueForm type FROM that zod Schema using the z.infer<> methdod
+ */
+
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage: React.FC = () => {
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
   const [error, setError] = useState("");
+  const [success, setSuccess] = useState(false);
   const router = useRouter();
 
   const onSubmit = async (data: IssueForm) => {
     try {
       await axios.post("/api/issues", data);
-      router.push("/issues");
+      setSuccess(true);
+      setTimeout(() => {
+        router.push("/issues");
+      }, 2000);
     } catch (error: any) {
       console.log("Error encountered", error);
       setError("An unexpected error occurred.");
@@ -40,10 +57,24 @@ const NewIssuePage: React.FC = () => {
         </Callout.Root>
       )}
 
+      {success && (
+        <Callout.Root color="green">
+          <Callout.Icon>
+            <CiCircleInfo />
+          </Callout.Icon>
+          <Callout.Text>Issue created</Callout.Text>
+        </Callout.Root>
+      )}
+
       <form onSubmit={handleSubmit(onSubmit)} className="max-w-xl space-y-7">
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
+        {errors.title && (
+          <Text color="red" as="p">
+            {errors.title.message}
+          </Text>
+        )}
         {/* you can't use {...register("title")} for the simpleMDE as it doesn't allow any extra props, so we need to use the Controller from React Hook Form */}
         <Controller
           name="description"
@@ -52,6 +83,11 @@ const NewIssuePage: React.FC = () => {
             <SimpleMde placeholder="Description" {...field} />
           )}
         ></Controller>
+        {errors.description && (
+          <Text color="red" as="p">
+            {errors.description.message}
+          </Text>
+        )}
 
         <Button variant="soft">Submit issue</Button>
       </form>
